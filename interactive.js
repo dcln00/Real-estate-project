7d69bd51666f0bff6bcf9b46192fd3c441f3b535
dcln00 <nii.aryh@gmail.com>
2021-12-09 22:05:03 +0000

    WaveBox Realty Project

---
+++ interactive.js
@@ -0,0 +1,19 @@
+window.onscroll = function() {myFunction()};
+var navbar = document.getElementById("nav");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+} 
+
+function openNav() {
+  document.getElementById("mobileNav").style.width = "50%";
+}
+
+function closeNav() {
+  document.getElementById("mobileNav").style.width = "0%";
+}
